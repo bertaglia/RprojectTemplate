a2d0f4098267b55834869f3a31462d977e537e32
Carlos Bertaglia <bertaglia.carlos@gmail.com>
2019-04-01 11:23:50 -0300

    add some useful files and config yaml
    add some useful files and config yaml

--- RScripts/mytools.R
+++ RScripts/mytools.R
@@ -1,10 +1,15 @@
-#load all required packages
-requiredpackages <- c('lintr', 'ggplot2', 'styler')
+# load all required packages
+requiredpackages <- c("lintr", "ggplot2", "styler", "config")
 
 for (package in requiredpackages) {
-    if (!require(package, character.only=T)) {
-        install.packages(package)
-        library(package, character.only=T)
-    }
+  if (!require(package, character.only = T)) {
+    install.packages(package)
+    library(package, character.only = T)
+  }
 }
 
+# remember to keep your config.yml out of your vcs (eg: include it in your .gitignore)
+# exemplo how to get config from config.yml
+# my_awesome_api_configuration <- config::get("my_awesome_api")
+# my_awesome_api_configuration$url,
+# my_awesome_api_configuration$api_key
